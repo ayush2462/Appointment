aebbc5926b2b868528f2fc0178404a5badca6bba
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-09-27 16:29:25 +0530

    installing nodemon and updating server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
+
+app.get("/", (req, res) => {
+  res.send("hello world! Ayush");
+});
+app.listen(port, () => console.log("Server is Started", port));
