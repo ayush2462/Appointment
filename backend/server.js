34765de6c5555c9fd0a38406711b17cafd7a3a9c
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-09-28 23:48:03 +0530

    "Added Express.js setup, CORS, and MongoDB/Cloudinary connections to server.js"

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3000;
+connectDB();
+connectCloudinary();
+//middleware
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(port, () => {
+  console.log("Server connected to", port);
+});
