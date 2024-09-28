790f6bc9892214e5a0b7a3dcdb3baf5011e986e4
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-09-29 02:17:10 +0530

    "Added imports for validator, bcrypt, and cloudinary; implemented input validation, password hashing, and image upload to adminController's addDoctor function."

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,3 +1,7 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
 // API for adding doctor
 const addDoctor = async (req, res) => {
   try {
@@ -13,17 +17,59 @@ const addDoctor = async (req, res) => {
       address,
     } = req.body;
     const imageFile = req.file;
-    console.log({
+    // checking for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email ",
+      });
+    }
+
+    //validator strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password ",
+      });
+    }
+    // hashing doctor password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //upload cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
       name,
       email,
-      password,
+      image: imageUrl,
+      password: hashedPassword,
       speciality,
       degree,
       experience,
       about,
       fees,
-      address,
-    },imageFile);
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+    
   } catch (error) {}
 };
 export { addDoctor };
