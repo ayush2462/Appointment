12cdaf1b2e4afa6f84c6f92ac7297070f5a9b342
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-09-29 01:04:38 +0530

    "Added doctor model and admin route, updated server.js to include admin endpoint"

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -12,8 +13,13 @@ connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
+
+//api endpoint
+app.use('/api/admin',adminRouter)
+//localhost:3000/api/admin/add-doctor 
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world how are you");
 });
 app.listen(port, () => {
   console.log("Server connected to", port);
