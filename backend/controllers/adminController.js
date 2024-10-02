19f23fcb35b6d8d5fddbfbbfe28b14a772a071e6
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-10-03 01:25:31 +0530

    updating adminroute adminController and env

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,7 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
-
+import jwt from "jsonwebtoken";
 import doctorModel from "../models/doctorModel.js";
 const addDoctor = async (req, res) => {
   try {
@@ -77,4 +77,24 @@ const addDoctor = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
-export { addDoctor };
+
+// API for admin login
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credential" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, loginAdmin };
