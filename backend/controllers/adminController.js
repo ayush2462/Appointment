051b5a70de38a02e0f5c22526e517a8a98550902
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-10-03 01:06:46 +0530

    adding admincontroller updating env file and doctorModel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,8 +1,8 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
+
 import doctorModel from "../models/doctorModel.js";
-// API for adding doctor
 const addDoctor = async (req, res) => {
   try {
     const {
@@ -17,7 +17,7 @@ const addDoctor = async (req, res) => {
       address,
     } = req.body;
     const imageFile = req.file;
-    // checking for all data to add doctor
+    //checking for all data to add doctor
     if (
       !name ||
       !email ||
@@ -31,26 +31,26 @@ const addDoctor = async (req, res) => {
     ) {
       return res.json({ success: false, message: "Missing Details" });
     }
-    // validating email format
+    //validating email format
     if (!validator.isEmail(email)) {
       return res.json({
         success: false,
-        message: "Please enter a valid email ",
+        message: "Please enter a valid email",
       });
     }
 
-    //validator strong password
+    // validating strong password
     if (password.length < 8) {
       return res.json({
         success: false,
-        message: "Please enter a strong password ",
+        message: "Please enter a strong password",
       });
     }
     // hashing doctor password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //upload cloudinary
+    // upload image to cloudinary
     const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
       resource_type: "image",
     });
@@ -69,7 +69,12 @@ const addDoctor = async (req, res) => {
       address: JSON.parse(address),
       date: Date.now(),
     };
-    
-  } catch (error) {}
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+    res.json({ success: true, message: "Doctor Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 export { addDoctor };
