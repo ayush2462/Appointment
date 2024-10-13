c4eeaf2b864f3e2dca9c763d98f07f44fb0979ee
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-10-13 12:17:41 +0530

    "Added admin context with token and doctor list, implemented get all doctors API, and updated admin routes and pages"

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -96,5 +96,14 @@ const loginAdmin = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
-
-export { addDoctor, loginAdmin };
+//API to get all doxctor list for admin panel
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+export { addDoctor, loginAdmin, allDoctors };
