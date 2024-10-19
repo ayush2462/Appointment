408ce940a7d3a155d51c7affb3c92e3f26aee4fd
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-10-19 23:16:45 +0530

    "Added userRouter to server.js and mounted it at /api/user"

---
+++ backend/controllers/userController.js
@@ -0,0 +1,69 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({
+        success: false,
+        message: "Missing Details",
+      });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Enter a valid Email address",
+      });
+    }
+    //validation password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password  must be at least 8 characters long and stong",
+      });
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API for user Login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Password" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+export { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app.use(cors());
 app.use("/api/admin", adminRouter);
 //localhost:3000/api/admin/add-doctor
 app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
 app.get("/", (req, res) => {
   res.send("hello world how are you");
 });
