5173d8cdb9226941cb2570089813b4be11c0360f
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-10-19 22:03:17 +0530

    "Updated AdminContext to add changeAvailability function and modified DoctorsList component to use it"

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -2,25 +2,28 @@ import React, { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
 
 const DoctorsList = () => {
-  const { doctors, getAllDoctors, aToken } = useContext(AdminContext);
+  const { doctors, getAllDoctors, aToken, changeAvailability } =
+    useContext(AdminContext);
+  
   useEffect(() => {
     if (aToken) {
       getAllDoctors();
     }
   }, [aToken]);
+  
   return (
     <div className="m-5 max-h-[90vh] overflow-y-scroll ">
       <h1 className="text-lg font-medium ">All Doctors</h1>
       <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
-        {doctors.map((item, i) => (
+        {doctors.map((item) => (
           <div
             className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group"
-            key={i}
+            key={item._id} // Use item._id as the key
           >
             <img
               className="bg-indigo-50 group-hover:bg-primary transition-all duration-500"
               src={item.image}
-              alt=""
+              alt={`Profile of Dr. ${item.name}`} // Descriptive alt text
             />
             <div className="p-4">
               <p className="text-neutral-800 text-lg font-medium ">
@@ -28,7 +31,11 @@ const DoctorsList = () => {
               </p>
               <p className="text-zinc-600 text-sm ">{item.speciality}</p>
               <div className="mt-2 flex items-center gap-1 text-sm">
-                <input type="checkbox" checked={item.available} />
+                <input
+                  onChange={() => changeAvailability(item._id)}
+                  type="checkbox"
+                  checked={item.available}
+                />
                 <p>Available</p>
               </div>
             </div>
